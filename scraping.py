c16b4d49640f3ae7ed23e9a1f09ee9ae19a33f58
Natalee Turner <natalee@Natalees-Air.attlocal.net>
2021-11-01 11:38:48 -0500

    Challenge deliverable 3 part 2

--- scraping.py
+++ scraping.py
@@ -96,7 +96,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-striped table bordered")
 
 def mars_hemispheres(browser):
     # 1. Use browser to visit the URL 
